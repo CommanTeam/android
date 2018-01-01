34b5aeae9c44f7e72526b740cf0446aaa1fef2c4
DESKTOP-6CGBNE9\KSY <DESKTOP-6CGBNE9\KSY>
2018-01-01 18:36:00 +0900

    cardnews 중간 과정

--- Comman/app/src/main/java/org/appjam/comman/Fragment/SecondFragActivity.kt
+++ Comman/app/src/main/java/org/appjam/comman/Fragment/SecondFragActivity.kt
@@ -1,20 +1,19 @@
 package org.appjam.comman.Fragment
 
+import android.app.Fragment
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.appjam.comman.R
 
 /**
- * Created by KSY on 2017-12-31.
+ * Created by KSY on 2018-01-01.
  */
-class FirstFragActivity : Fragment(){
+class   SecondFragActivity :Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = inflater!!.inflate(R.layout.fragment_card_first, container, false)
+        val v = inflater!!.inflate(R.layout.fragment_card_second, container, false)
 
         return v
     }
 }
-
--- Comman/app/src/main/java/org/appjam/comman/ViewPager/CardActivity.kt
+++ Comman/app/src/main/java/org/appjam/comman/ViewPager/CardActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import org.appjam.comman.R
 
 /**
@@ -15,40 +16,8 @@ class CardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun AddFragment(fragment: Fragment, bundle: Bundle, tag: String, fragment2: Fragment) {
-            val fm = supportFragmentManager               // 프레그먼트 매니져 객체를 생성
-            val transaction = fm.beginTransaction()        // 트랜젝션 객체를 생성 프레그먼트 매니져 클래스를 통해 해당 트랙젝션을 수행하겠다는 의미
-            transaction.remove(fragment2)                                  // 기존 프레그먼트 제거
-            fragment.arguments = bundle                                 // bundle 객체를 넘겨준다
-            transaction.add(R.id.main2_container, fragment, tag)         // 새로운 프레그먼트 추가!!
-            //transaction.addToBackStack(null);                             // 백스텍에 저장!!
-            transaction.commit()
+        main_viewpager.currentItem = 0
 
-
-
-
-
-
-        }
-
-        fun AddFragment(fragment: Fragment, bundle: Bundle, tag: String) {            //오버라이딩하여 최초에 추가될 프레그먼트 생성함수
-            val fm = supportFragmentManager
-            val transaction = fm.beginTransaction()
-            fragment.arguments = bundle
-            transaction.add(R.id.main2_container, fragment, tag)
-            //transaction.addToBackStack(null);
-            transaction.commit()
-        }
-
-        fun TagFragment(tag: String) {
-            Log.v("home", tag)
-            val fm = supportFragmentManager
-            val transaction = fm.beginTransaction()
-            val fragment = supportFragmentManager.findFragmentByTag(tag)
-            if (fragment != null) {
-                transaction.replace(R.id.main2_container, fragment)
-                transaction.commit()
-            }
-        }
+    }
     }
 }
