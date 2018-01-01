dcfd5b984f8d1c6c04caaeef616c83d2988e3dc4
RyuDongIl <jules010@naver.com>
2018-01-01 10:39:27 +0900

    일단 중간부분까지

---
+++ Comman/app/src/main/java/org/appjam/comman/QuizActivity.kt
@@ -0,0 +1,12 @@
+package org.appjam.comman
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
---
+++ Comman/app/src/main/java/org/appjam/comman/QuizSubmitFragment.kt
@@ -0,0 +1,20 @@
+package org.appjam.comman
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by RyuDongIl on 2017-12-31.
+ */
+class QuizSubmitFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater!!.inflate(R.layout.fragment_quiz_choice, container, false)
+        if(arguments != null) {
+
+        }
+        return v
+    }
+}
