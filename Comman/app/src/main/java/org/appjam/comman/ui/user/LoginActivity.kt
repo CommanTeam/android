e98ca723bf948d4a35d991826562a259c342d3a6
seoyeon <syeon970204@duksung.ac.kr>
2018-01-11 06:29:51 +0900

    썸네일 적용

--- Comman/app/src/main/java/org/appjam/comman/ui/user/LoginActivity.kt
+++ Comman/app/src/main/java/org/appjam/comman/ui/user/LoginActivity.kt
@@ -29,7 +29,7 @@ import kotlinx.android.synthetic.main.activity_login.*
 import org.appjam.comman.R
 import org.appjam.comman.network.APIClient
 import org.appjam.comman.network.data.LoginData
-import org.appjam.comman.ui.main.MainActivity
+import org.appjam.comman.ui.CourseSubsection.CourseSubActivity
 import org.appjam.comman.util.PrefUtils
 import org.appjam.comman.util.setDefaultThreads
 import java.security.MessageDigest
@@ -164,7 +164,7 @@ class LoginActivity : AppCompatActivity() {
                                 PrefUtils.putUserToken(this@LoginActivity, response.result.token)
                                 PrefUtils.putUserInfo(this@LoginActivity, response.result.user)
 
-                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                val intent = Intent(this@LoginActivity, CourseSubActivity::class.java)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
@@ -178,7 +178,7 @@ class LoginActivity : AppCompatActivity() {
                                 response ->
                                 PrefUtils.putUserToken(this@LoginActivity, response.result.token)
                                 PrefUtils.putUserInfo(this@LoginActivity, response.result.user)
-                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                val intent = Intent(this@LoginActivity, CourseSubActivity::class.java)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
